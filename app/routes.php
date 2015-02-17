ec4e1465f2992470fd2f12b14ae0bc7ec0cc7623
Denaton <dennisMagnusson_4@hotmail.com>
2015-02-17 15:18:13 +0100

    Merged Allot of things

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,12 @@
 |
 */
 
-Route::get('users', function()
+Route::get('/', function()
+{
+	return View::make('hello');
+});
+
+Route::get('/users', function()
 {
 	//return View::make('hello');
 	//$users = User::has('project')->get();
