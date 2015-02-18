b5c5c1535d8b9d94a68787973b93c8ee0e70046c
magralol <gnorr_92@hotmail.com>
2015-02-18 10:16:02 +0100

    Added a S to statu tag

--- app/views/doc.blade.php
+++ app/views/doc.blade.php
@@ -12,7 +12,7 @@
       <input class="option" type="radio" name="option" value="project"><lable>Projekt</lable>
       <input class="option" type="radio" name="option" value="experince"><lable>Erfarenheter</lable>
       <input class="option" type="radio" name="option" value="category"><lable>Kategorier</lable>
-      <input class="option" type="radio" name="option" value="statu"><lable>Status</lable>
+      <input class="option" type="radio" name="option" value="status"><lable>Status</lable>
     </form>
     <form id="keysform" style="display:none">
       <h2>Vad söker du efter?</h2>
@@ -20,7 +20,7 @@
       <input class="key" type="checkbox" value="project"><lable>Projekt</lable>
       <input class="key" type="checkbox" value="experince"><lable>Erfarenheter</lable>
       <input class="key" type="checkbox" value="category"><lable>Kategorier</lable>
-      <input class="key" type="checkbox" value="statu"><lable>Status</lable>
+      <input class="key" type="checkbox" value="status"><lable>Status</lable>
     </form>
 
     <script>
