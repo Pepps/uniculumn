beeda68c4eda171b42297a89fb3f07fdb4fc76bf
magralol <gnorr_92@hotmail.com>
2015-02-18 10:14:34 +0100

    Changed form plural to singular

--- app/views/doc.blade.php
+++ app/views/doc.blade.php
@@ -8,19 +8,19 @@
     <p id="url">http://www.unicolum.se/search/</p>
     <form>
       <h2>Vad vill du söka i?</h2>
-      <input class="option" type="radio" name="option" value="users"><lable>Användare</lable>
-      <input class="option" type="radio" name="option" value="projects"><lable>Projekt</lable>
-      <input class="option" type="radio" name="option" value="experinces"><lable>Erfarenheter</lable>
-      <input class="option" type="radio" name="option" value="categorys"><lable>Kategorier</lable>
-      <input class="option" type="radio" name="option" value="status"><lable>Status</lable>
+      <input class="option" type="radio" name="option" value="user"><lable>Användare</lable>
+      <input class="option" type="radio" name="option" value="project"><lable>Projekt</lable>
+      <input class="option" type="radio" name="option" value="experince"><lable>Erfarenheter</lable>
+      <input class="option" type="radio" name="option" value="category"><lable>Kategorier</lable>
+      <input class="option" type="radio" name="option" value="statu"><lable>Status</lable>
     </form>
     <form id="keysform" style="display:none">
       <h2>Vad söker du efter?</h2>
-      <input class="key" type="checkbox" value="users"><lable>Användare</lable>
-      <input class="key" type="checkbox" value="projects"><lable>Projekt</lable>
-      <input class="key" type="checkbox" value="experinces"><lable>Erfarenheter</lable>
-      <input class="key" type="checkbox" value="categorys"><lable>Kategorier</lable>
-      <input class="key" type="checkbox" value="status"><lable>Status</lable>
+      <input class="key" type="checkbox" value="user"><lable>Användare</lable>
+      <input class="key" type="checkbox" value="project"><lable>Projekt</lable>
+      <input class="key" type="checkbox" value="experince"><lable>Erfarenheter</lable>
+      <input class="key" type="checkbox" value="category"><lable>Kategorier</lable>
+      <input class="key" type="checkbox" value="statu"><lable>Status</lable>
     </form>
 
     <script>
