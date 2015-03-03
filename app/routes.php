236e31792b5a68e3a366718357a1310f9097b661
magralol <gnorr_92@hotmail.com>
2015-03-03 10:26:14 +0100

    made some minor changes to routes.php

--- app/routes.php
+++ app/routes.php
@@ -3,8 +3,6 @@
 Route::get("/", "AuthController@googleStatus");
 Route::get('/', 'HomeController@index');
 
-
-//
 Route::get('/login', array('as' => 'login', 'uses' => 'AuthController@loginWithGoogle'));
 //Route for getting login information
 Route::post('/home', array('as' => 'home', 'uses' => 'AuthController@logIn'));
@@ -21,14 +19,15 @@ Route::get("/search/{option}/{key}/{val}", "SearchController@index");
 Route::get("/search/{option}/{key}/{val}/{pretty}", "SearchController@index");
 
 Route::resource('project', "ProjectController");
-Route::post('apply/upload', 'ProjectController@store');
+Route::post('/apply/upload', 'ProjectController@store');
 /* Duck punch for fixing update */
 Route::post('/project/update/{id}', "ProjectController@update");
 Route::get('/project/delete/{id}', "ProjectController@destroy");
+
 Route::get('/project/showfiles/{id}', "ProjectController@showfiles");
 Route::get('/project/getfiles/{id}', "ProjectController@getfiles");
 Route::get('/project/readfile/{id}', "ProjectController@readfile");
 
-Route::get('category/show/{id}', 'CategoryController@show');
+Route::get('/category/show/{id}', 'CategoryController@show');
 
 Route::get('/register', 'HomeController@index');
