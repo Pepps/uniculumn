ef52065efe56fadd58ae95aad90afc7504dad50b
magralol <gnorr_92@hotmail.com>
2015-03-19 14:38:30 +0100

    style stuff

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -17,6 +17,7 @@
   			<img src="img/penna.png"><img src="img/ladda.png"><img src="img/gubbe.png">
   			<h1>Ladda upp projekt och n&auml;tverka med arbetsgivare</h1></div>
   <div id="loginbox">
+
   		<form id="form" name="form" method="post" action=" ">
   			E-Mail<br><input type="text" name="username" id="username"/><br>
   			L&ouml;senord<br><input type="password" name="password" id="password"/><br><br>
@@ -30,4 +31,20 @@
       {{ HTML::image('img/big_logo.png', 'Big logo', Array('id' => 'abigloggo')) }}
   </div>
 </div>
+
+<script>
+  window.onload =  function(){
+    $("#loggin-btn").on("click", function(){
+      $.ajax({
+        method: "POST",
+        url: "/home",
+        data: { email: $("#username").val(), password: $("#password").val() }
+      }).done(function(data) {
+        window.location.replace("/")
+        }).fail(function() {
+          console.error("Something went wrong!");
+        });
+    });
+  }
+</script>
 @stop
