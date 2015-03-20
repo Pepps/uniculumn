0f1c5a5fb98ec4cc9bab6d67fb4e42511211e52b
magralol <gnorr_92@hotmail.com>
2015-03-20 10:44:34 +0100

    squashed a small bugg

--- app/views/user/edit.blade.php
+++ app/views/user/edit.blade.php
@@ -122,9 +122,8 @@
       {{ Form::open(array('url' => 'user/update_interest/'.$user->id)) }}
       <div class="account_column"  style="padding-left: 3vw;">
              <div class="dark_icon user_interests"> </div><h3>Lägg till intressen</h3>
-              {{$categories = Category::all()}}
               <select class="uploadfile" id="categories-select">
-                     @foreach($categories as $category)
+                     @foreach(Category::all() as $category)
                       @if ($category->subcategory_id == 0)
                         <option value="{{ $category->id }}" >{{ $category->title }}</option>
                       @endif
